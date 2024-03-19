c4a876c02b339917ad3f8d6b67c21d4d91bd1722
VaibhavBansa1 <vaibhavbansalgwl@gmail.com>
2024-03-19 23:00:58 +0530

    Front page

--- index.php
+++ index.php
@@ -6,32 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <style>
-        body{
-            font-family:Arial,sans-serif;
-            margin:0;
-            padding:0;
-            background-color:#f0f0f0;
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f0f0f0;
         }
-        nav{
+
+        nav {
             background-color: #0f0f0f;
-            padding:10px;
+            padding: 10px;
             text-align: center;
         }
-        nav ul{
+
+        nav ul {
             list-style-type: none;
             text-decoration: none;
         }
-        nav ul li a{
-            color:#f0f0f0;
+
+        nav ul li a {
+            color: #f0f0f0;
             text-decoration: none;
             padding: 5px;
         }
-        nav ul li a:hover{
-            background-color:#555;
+
+        nav ul li a:hover {
+            background-color: #555;
         }
-        nav ul li{
+
+        nav ul li {
             display: inline;
-            margin-right:10px;
+            margin-right: 10px;
         }
     </style>
 </head>
