4f820b3fb2186a52f3de405bc476a195fa360343
VaibhavBansa1 <vaibhavbansalgwl@gmail.com>
2024-03-24 00:48:26 +0530

    correction

--- Student management/faculty/login.php
+++ Student management/faculty/login.php
@@ -2,19 +2,10 @@
     if(isset($_POST['id']) && isset($_POST['password']) && isset($_POST['submit'])){
         $id = $_POST['id'] ;
         $password = $_POST['password'];
-
-        $host="localhost";
-        $port=3306;
-        $socket="";
-        $user="root";
-        $pass="";
-        $dbname="college";
-        
-        $con = new mysqli($host, $user, $pass, $dbname, $port, $socket)
-        or die ('Could not connect to the database server' . mysqli_connect_error());
-        
-        $sql = "SELECT id, password FROM student WHERE password = '$password' AND id = '$id';";
-        $result = $con->query($sql)->fetch_assoc();
+        // connection is in conn.php
+        include("conn.php");
+        $sql = "SELECT id, password FROM faculty WHERE password = '$password' AND id = '$id';";
+        $result = $conn->query($sql)->fetch_assoc();
         echo $result['password'], $password;
         if (($result['password'] === $password) && ($result['id'] === $id) ){
             header("location:studentdetail.php");
@@ -23,7 +14,7 @@
             header("location:index.php?fail=".true);
         }
 
-        $con->close();
+        $conn->close();
     }
     else{
         header("Location:index.php");
