8d2c406d47439125e42e744746d4aa88e28b71a1
keshavrajpoot2007 <keshavrajpoot2007@gmail.com>
2024-06-02 20:13:04 +0530

    improve  search option

--- Student management/faculty/students_detail.php
+++ Student management/faculty/students_detail.php
@@ -232,9 +232,9 @@ include '../conn.php';
     <?php
         include '../footer.php';
     ?>
-        <script>
-        function filterTable() {
-            var input, filter, table, tr, td, i, txtValue;
+    <script>
+        function filterTable(){
+            var input, filter, table, tr, td, i, j, txtValue;
             input = document.getElementById("myInput");
             filter = input.value.toUpperCase();
             table = document.getElementById("myTable");
@@ -242,18 +242,20 @@ include '../conn.php';
 
             // Loop through all table rows, and hide those who don't match the search query
             for (i = 0; i < tr.length; i++) {
-                td = tr[i].getElementsByTagName("td")[0];
-                if (td) {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                } else {
-                    tr[i].style.display = "none";
-                }
+                for(j=0;j<8;j++){
+                    td = tr[i].getElementsByTagName("td")[j];
+                    if (td) {
+                        txtValue = td.textContent || td.innerText;
+                        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                            tr[i].style.display = "";
+                            break;
+                        } else {
+                            tr[i].style.display = "none";
+                        }
+                    }
                 } 
-            }
+            }    
         }
-
     </script>
 </body>
 
