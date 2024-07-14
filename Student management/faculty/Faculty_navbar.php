93df7db6573a2624ced8325e5416cbd234687638
VaibhavBansa1 <vaibhavbansalgwl@gmail.com>
2024-07-14 20:33:26 +0530

    correction

--- Student management/faculty/Faculty_navbar.php
+++ Student management/faculty/Faculty_navbar.php
@@ -32,7 +32,7 @@
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="../faculty/Faculty_home.php">Home</a>
+                            <a class="nav-link active" aria-current="page" href="../faculty/faculty_home.php">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="../faculty/students_detail.php">Students Data</a>
